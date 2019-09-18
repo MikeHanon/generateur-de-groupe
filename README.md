05f1510fa932c472dd69ef6546cb5179697f59ca
Mikhaïl Hanon <mike.hanon@gmail.com>
2019-09-18 11:17:11 +0200

    mise en ligne du projet

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# generateur-de-groupe
-Générateur de groupe aléatoire
+# Générateur de groupe aléatoire
+
+Réalisation d'un générateur de groupe pour la formation Becode. On commence par rentrer le nombre total de participant, ensuite on rentre leur nom et on termine par le nombre de groupe souhaité. Le script vas d'abord mélangé les noms avant de les sortir en groupe.
+
+<img src="screenshot.png">
+
+## Construit avec
+
+Javascript
+
+## Autheurs
+
+* **Hanon Mikhaïl** MikeHanon (https://github.com/MikeHanon)
+
---
+++ repartitonGroup.js
@@ -0,0 +1,36 @@
+var list = []
+count = 0
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+function assign() {
+    let people=parseInt(prompt('Entrez le nombre de participant'))
+    let name = prompt("Entrez un nom")
+    list.push(name)
+    while(list.length !== people){
+        let name= prompt("Entrez un nom")
+        list.push(name)
+        
+    }
+shuffle(list)
+var nbrgrp=parseInt(prompt("entrez le nombre de groupe"));
+// for(i=0; i < nbrgrp; i++){
+//     document.getElementById('allgroup').innerHTML = `<p id=group${i}><p>\n`
+//     console.log(i, 2)
+   
+// }
+var group= Math.floor(list.length/nbrgrp)
+for(i=0; i <= nbrgrp; i++){
+let tab=list.slice(count,group)
+list.splice(count, group)
+document.getElementsByTagName("p")[0].innerHTML = `${document.getElementsByTagName("p")[0].innerHTML}group${i} : ${tab} <br>`
+
+console.log(tab, "tableau group")
+}
+}
